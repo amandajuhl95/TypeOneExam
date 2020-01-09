b3767ec3e99888b249f42dd2598f11f75f40fddf
Amanda <Manda_Hansen@hotmail.com>
2020-01-09 19:05:39 +0100

    travis

--- src/main/java/security/LoginEndpoint.java
+++ src/main/java/security/LoginEndpoint.java
@@ -55,6 +55,7 @@ public class LoginEndpoint {
         throw (AuthenticationException) ex;
       }
       Logger.getLogger(GenericExceptionMapper.class.getName()).log(Level.SEVERE, null, ex);
+      
       throw new AuthenticationException(ex.getMessage());
     }
     
